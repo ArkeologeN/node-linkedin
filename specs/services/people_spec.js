af7660a6f588aa88e23985bb64542be0688c5193
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-10-04 02:13:54 -0400

    Add spec for new People Invite API.

--- specs/services/people_spec.js
+++ specs/services/people_spec.js
@@ -3,7 +3,7 @@
  * @since   2/9/14
  */
 
-var linkedin = require('../../')('77pmiwaj7b1ofn', 'EKKZCDTL7IiNUqra')
+var linkedin = require('../../')('75gyccfxufrozz', 'HKwSAPg0z7oGYfh5')
 token = process.env.IN_TOKEN;
 
 jasmine.getEnv().defaultTimeoutInterval = 20000;
@@ -12,9 +12,32 @@ linkedin = linkedin.init(token);
 
 describe('API: People Test Suite', function() {
 
-    it('should retrieve profile of current user', function(done ) {
-       linkedin.people.me(function(err, data) {
-           done();
-       });
+    it('should retrieve profile of current user', function(done) {
+        linkedin.people.me(function(err, data) {
+            done();
+        });
     });
-})
+
+    it('should invite someone to connect', function(done) {
+        linkedin.people.invite({
+            "recipients": {
+                "values": [{
+                    "person": {
+                        "_path": "/people/email=glavin.wiechert@gmail.com",
+                        "first-name": "Glavin",
+                        "last-name": "Wiechert"
+                    }
+                }]
+            },
+            "subject": "Invitation to connect.",
+            "body": "Say yes!",
+            "item-content": {
+                "invitation-request": {
+                    "connect-type": "friend"
+                }
+            }
+        }, function(err, data) {
+            console.log(err, data);
+        });
+    });
+});
