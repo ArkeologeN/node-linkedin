d8a9a74508e61ef311ad8ec259537c0a3492213e
ArkeologeN <hamzawaqas.programmer@gmail.com>
2018-01-29 18:11:02 +0100

    organizationAccessControlInfo method added in organization-entity

--- src/services/organizationEntity.js
+++ src/services/organizationEntity.js
@@ -13,6 +13,21 @@ class OrganizationEntity extends APIService {
 		let url = `/organizationalEntityAcls?` + query.join('&');
 		return this.get({url});
 	}
+
+	organizationAccessControlInfo({organizationalTarget, role, state} = {}) {
+		if (!organizationalTarget) {
+			throw new Error(`[organizationAccessControlInfo] organizationTarget is required`);
+		}
+		let query = [`q=organizationalTarget`, `organizationalTarget=${organizationalTarget}`];
+		if (role) {
+			query.push(`role=${role}`);
+		}
+		if (state) {
+			query.push(`state=${state}`);
+		}
+		let url = `/organizationalEntityAcls?` + query.join('&');
+		return this.get({url});
+	}
 }
 
 module.exports = OrganizationEntity;
--- tests/tdd/services/organizationEntity.js
+++ tests/tdd/services/organizationEntity.js
@@ -6,7 +6,7 @@ const OrganizationEntity = require('../../../src/services/organizationEntity');
 const ApiService = require('../../../src/apiService');
 chai.use(require('chai-as-promised'));
 
-describe.only('#organizationEntity', () => {
+describe('#organizationEntity', () => {
 
   it('should be extended with `APIService`', () => {
     expect(OrganizationEntity.prototype).to.be.an.instanceOf(ApiService);
@@ -29,6 +29,10 @@ describe.only('#organizationEntity', () => {
 
     it('should have a method `memberAccessControlInfo`', () => {
       expect(organizationEntity.memberAccessControlInfo).to.be.a('function');
+    });
+
+    it('should have a method `organizationAccessControlInfo`', () => {
+	    expect(organizationEntity.organizationAccessControlInfo).to.be.a('function');
     });
 
 	  context('When `memberAccessControlInfo` method is called', () => {
@@ -65,6 +69,57 @@ describe.only('#organizationEntity', () => {
 		  it('should invoke `request` with role & state', async () => {
 			  const expectedUrl = `/organizationalEntityAcls?q=roleAssignee&role=${role}&state=${state}`;
 			  await organizationEntity.memberAccessControlInfo({role, state});
+			  expect(organizationEntity.request.args).to.deep.equal([
+				  [
+					  {
+						  method: 'GET',
+						  opts: {},
+						  url: expectedUrl
+					  }
+				  ]
+			  ]);
+		  });
+	  });
+
+	  context('When `organizationAccessControlInfo` method is called', () => {
+		  const [organizationalTarget, role, state] = ['urn:li:organization:1337','ADMINISTRATOR', 'APPROVED'];
+
+		  it('should raise an exception for missing `organizationTarget`', async () => {
+			  const wrapFn = () => organizationEntity.organizationAccessControlInfo();
+			  expect(wrapFn).to.throw(/organizationTarget/);
+		  });
+
+		  it('should invoke `request` with organizationTarget', async () => {
+		  	const expectedUrl = `/organizationalEntityAcls?q=organizationalTarget&organizationalTarget=${organizationalTarget}`;
+		  	await organizationEntity.organizationAccessControlInfo({organizationalTarget});
+			  expect(organizationEntity.request.args).to.deep.equal([
+				  [
+					  {
+						  method: 'GET',
+						  opts: {},
+						  url: expectedUrl
+					  }
+				  ]
+			  ]);
+		  });
+
+		  it('should invoke `request` with organizationTarget & role', async () => {
+			  const expectedUrl = `/organizationalEntityAcls?q=organizationalTarget&organizationalTarget=${organizationalTarget}&role=${role}`;
+			  await organizationEntity.organizationAccessControlInfo({organizationalTarget, role});
+			  expect(organizationEntity.request.args).to.deep.equal([
+				  [
+					  {
+						  method: 'GET',
+						  opts: {},
+						  url: expectedUrl
+					  }
+				  ]
+			  ]);
+		  });
+
+		  it('should invoke `request` with organizationTarget, role and state', async () => {
+			  const expectedUrl = `/organizationalEntityAcls?q=organizationalTarget&organizationalTarget=${organizationalTarget}&role=${role}&state=${state}`;
+			  await organizationEntity.organizationAccessControlInfo({organizationalTarget, role, state});
 			  expect(organizationEntity.request.args).to.deep.equal([
 				  [
 					  {
